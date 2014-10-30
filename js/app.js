e547b0dace2a973b2fddd2a9ca10d2a005f5d5e2
quincyp <quincytpham@gmail.com>
2014-10-30 09:26:23 -0700

    Added gameboard, added lodash.js, added attr to tile to change picture

--- js/app.js
+++ js/app.js
@@ -4,6 +4,43 @@
 
 "use strict";
 
+var tile = 'img/sec1.jpg'//'any kind of data assoc with element';
+var info = 'some other data';
+
+var gameBoard = $('#game-board');
+
+//create and configure a new <img> element...
+var newTile = $(document.createElement('img'));
+
+newTile.attr('src', 'img/background.jpg');
+newTile.attr('alt', 'photo of nature');
+newTile.attr('width', '250px');
+
+
+//use the .data() method to associate extra data with HTML elements
+newTile.data('assocTile', tile) //first param is a key
+newTile.data('tileInfo', info) //second param is a value
+
+//add to the board
+gameBoard.append(newTile);
+
+//when an image in game-board is cicked
+$('#game-board img').click(function(){
+	//remember that 'this' refers to the DOM element that raised the event
+	//wrapped in jQuery $() to get more functionality
+	var clickedImage = $(this);
+	var tileData1 = clickedImage.data('assocTile');//var to data
+	var tileData2 = clickedImage.data('tileInfo');
+
+	clickedImage.attr('src', tileData1);//on event chang attr data
+	clickedImage.attr('alt', tileData2);
+
+	console.log(tileData1);//shows in log
+	console.log(tileData2);
+});
+
+
+
 //select all navigation links
 var allNavLinks = $('nav a');
 
